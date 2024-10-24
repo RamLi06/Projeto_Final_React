5587f88b9d51ba65cd4d7044152db6c857a3b7dd
Ramon Lima Rocha <ramon.lrocha@dinfor.uniceub>
2024-10-24 18:31:46 -0300

    Termina vagabundo

--- vsf/src/App.jsx
+++ vsf/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  function incrementar(){
+
+      setCount(count + 1);
+  }
+  function decrementar(){
+
+    setCount(count - 1);
 }
 
+  return (
+    <>
+      <h1>Contador</h1>
+    <div id ='botão'>
+      <button className='botao' onClick={incrementar}>Incrementar</button>
+      <button className='botao' onClick={decrementar}>Decrementar</button>
+      <p>{count}</p>
+      </div>
+   </>
+   )
+   
+  }
 export default App
